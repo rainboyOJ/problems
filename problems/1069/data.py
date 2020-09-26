d4354690d3a1de3cde4bb108fcfd427dfcdb25c0
rainboy <rainboylvx@qq.com>
2020-09-26 16:53:02 +0800

    fix

--- problems/1069/data.py
+++ problems/1069/data.py
@@ -4,7 +4,7 @@ from cyaron import * # 引入CYaRon的库
 
 for i in range(1,11):
     io = IO(file_prefix="problem", data_id=i) # test3.in, test3.out
-    a = randint(-10000,10000)
-    b = randint(2,100)
+    a = randint(-100,100)
+    b = randint(1,3)
     io.input_writeln(a,b)
     io.output_gen("../std")
