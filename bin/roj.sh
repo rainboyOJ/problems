4db8c41e4298cfd2c7639f45d455eceff9beaab9
rainboy <rainboylvx@qq.com>
2024-01-31 21:00:06 +0800

    upd roj.sh

--- bin/roj.sh
+++ bin/roj.sh
@@ -9,7 +9,6 @@
 ## 2.编译程序
 ## 3.评测
 
-
 ################### 评测函数
 
 # 创建一个评测的函数
@@ -82,43 +81,69 @@ judge_run() {
 	# echo $ret
 }
 
-
 # 查找名字含有相同字符串的文件
-# 
+#
 function find_file {
-    local find_str
-    find_str=$1
-    shift 1
-    local FZF_OPTIONS="--no-sort --layout=reverse --height 40% --border --margin=0,1"
-    local files
-    files="$(find . -type f -maxdepth 1 -name "$find_str" -printf "%f\n")"
-    if [ -z "$files" ];then
-      echo ""
-    else
-      echo "$files" | sort -f -i -t '.' -k 1 | fzf $* $FZF_OPTIONS
-    fi
+	local find_str
+	find_str=$1
+	shift 1
+	local FZF_OPTIONS="--no-sort --layout=reverse --height 40% --border --margin=0,1"
+	local files
+	files="$(find . -type f -maxdepth 1 -name "$find_str" -printf "%f\n")"
+	if [ -z "$files" ]; then
+		echo ""
+	else
+		echo "$files" | sort -f -i -t '.' -k 1 | fzf $* $FZF_OPTIONS
+	fi
 }
 #########################
 
 ## 0. 获取参数
-if [[ -z "$1" ]];then
-    echo "please input problem pid ,like 1000"
-    exit 1
+if [[ -z "$1" ]]; then
+	echo "please input problem pid ,like 1000"
+	exit 1
 fi
 
 ## 全局变量
 
-pid="$1"    #题号
+pid="$1"   #题号
 program="" # 评测程序
 roj_url="http://data.roj.ac.cn/"
 roj_data_url="${roj_url}${pid}"
 data_dir="data"
 
-if [[ -n "$2" ]];then
-    program="$2"
+if [[ -n "$2" ]]; then
+	program="$2"
 fi
 
-## 1. 下载数据
+## 1. 编译
+
+### 没有源文件的情况
+if [ -z "$program" ]; then
+	# program=$(find_file "*.out" "-1") # -1 自动选择一个元素,如果只有一个
+	FZF_OPTIONS="--no-sort --layout=reverse --height 40% --border --margin=0,1 -1"
+	files="$(find .  -maxdepth 1 -type f \( -name "*.cpp" -o -name "*.out" \) -printf "%f\n")"
+	if [ -z "$files" ]; then
+		echo "当前没有任何.out或.cpp文件"
+		exit 0
+	else
+		program=$(echo "$files" | sort -f -i -t '.' -k 1 | fzf $FZF_OPTIONS)
+	fi
+fi
+
+if [[ "$program" == *".cpp" ]];then
+	out="${program%.cpp}.out"
+	echo $program
+	echo $out
+	g++ -g -o $out $program
+	if [[ $? -ne 0 ]] ;then
+		echo "编译失败!!"
+		exit 1
+	fi
+	program=$out
+fi
+
+## 2. 下载数据
 
 function download_data {
 	wget -O tmp.zip "$roj_data_url"
@@ -126,18 +151,18 @@ function download_data {
 	/usr/bin/rm tmp.zip
 }
 
-if [[ -d $data_dir ]];then
+if [[ -d $data_dir ]]; then
 	read -r -p "$data_dir 已经存在,是否删除,然后重新下载数据?(y/N)" delete_data_dir
-	if [[ $delete_data_dir == "y" ]] || [[ $delete_data_dir == "Y" ]] ;then
-	   rm -rf $data_dir
-	   download_data
+	if [[ $delete_data_dir == "y" ]] || [[ $delete_data_dir == "Y" ]]; then
+		rm -rf $data_dir
+		download_data
 	fi
 else
 	download_data
 fi
 
 in_data=$(find $data_dir -type f -name "*.in" -printf '%f\n')
-out_data=$(find $data_dir -type f \( -name "*.out" -o -name "*.ans" \) -printf '%f\n' )
+out_data=$(find $data_dir -type f \( -name "*.out" -o -name "*.ans" \) -printf '%f\n')
 oneFileName=$(echo $in_data | cut -d ' ' -f1)
 first_num_pos=$(expr index $oneFileName $(grep -oP '\d+' <<<$oneFileName))
 #转数组
@@ -145,14 +170,6 @@ in_arr=($(echo $in_data | xargs -n 1 | sort -n -k 1.$first_num_pos))
 out_arr=($(echo $out_data | xargs -n 1 | sort -n -k 1.$first_num_pos))
 count_in=${#in_arr[*]}
 count_out=${#out_arr[*]}
-
-## 2. 编译
-
-
-### 没有源文件的情况
-if [ -z "$program" ]; then
-    program=$(find_file "*.out" "-1") # -1 自动选择一个元素,如果只有一个
-fi
 
 
 ## 3. 评测
