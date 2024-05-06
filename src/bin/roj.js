666ed9bafe1dbca752360c5c3dbf262664ef0b91
rainboy <rainboylvx@qq.com>
2024-05-06 09:07:32 +0800

    添加 roj.js命令行功能,通过命令行来管理本题库

---
+++ src/bin/roj.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// roj.js 是本题目库的命令行 使用[tj/commander  命令行参数解析库](https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md)
+// usage:
+// roj  --render-one <problem-path> 只渲染一个题目,注意不进行更新
+//
+
+console.log('hello')
